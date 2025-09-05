6df97421eb2eb9ac4497694a80ed0a3313c5b563
Lautaro Bertera <bertera.lautaro@gmail.com>
2025-09-05 20:58:38 -0300

    punteros

---
+++ punteros.c
@@ -0,0 +1,62 @@
+///////////PUNTEROS//////////
+
+
+//FORMATO PUNTERO
+//EJEMPLO
+//    tipo * nom_puntero;
+//
+//    NOM_PUNTERO es el identificador de la variable.
+//      *       es el operador que nos indica en la declaraciön, que la variable es de tipo puntero.
+//    TIPO:     es el tipo de variable apuntada por el puntero, denominado tipo base.
+
+// El tipo base, es muy importante, porque tiene incidencia en el comportamiento del puntero frente a la aritmética de punteros.
+
+
+//DECLARACION DE PUNTERO
+//EJEMPLO
+//    int x, *p;
+//
+// El * no se aplica a todos los nombres de variables de una declaraciön.
+// Cada puntero debe Ilevar su nombre precedido por *
+// En el ejemplo, "x" es un entero, pero "p" es un puntero a entero.
+//
+// char *puntero = NULL
+// Al crear *p, se crea con un valor desconocido es necesario inicializarlo con el valor adecuado. Es un  error comun y de ultima se pueda inicializarlo con NULL.
+
+
+//OPERADORES PUNTEROS & y *
+//EJEMPLO
+//    int x,*p;
+//    p=$x;
+//
+// & devuelve la direcciön de memoria de su operando. Con este operador, se puede relacionara los punteros con las variables a ser apuntadas.
+// El * es complementario al &, retorna el valor de la variable cuya direccion es la que apunta. Se opera con el contenido a lo que apunta la variable de tipo puntero.
+//
+// int main(){
+//    int *ptr;
+//    int dato=300;
+//
+//    ptr=&dato;
+//    *ptr=50;
+//
+//    printf("Dato = %d\n", dato);
+//
+//    return 0;
+// }
+// !!No hay que confundir el * que aparece en la declaracion con el operador de indireccion !!
+
+
+
+#include <stdio.h>
+
+struct
+
+
+void main(){
+
+
+
+}
+
+
+
