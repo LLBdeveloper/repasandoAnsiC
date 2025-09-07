e29565b36064564d3c7f96078cef88b32afafb92
Lautaro Bertera <bertera.lautaro@gmail.com>
2025-09-07 19:26:18 -0300

    punteros clase3

--- punteros.c
+++ punteros.c
@@ -20,7 +20,10 @@
 // Cada puntero debe Ilevar su nombre precedido por *
 // En el ejemplo, "x" es un entero, pero "p" es un puntero a entero.
 //
+//
+//EJEMPLO
 // char *puntero = NULL
+//
 // Al crear *p, se crea con un valor desconocido es necesario inicializarlo con el valor adecuado. Es un  error comun y de ultima se pueda inicializarlo con NULL.
 
 
@@ -32,6 +35,8 @@
 // & devuelve la direcciön de memoria de su operando. Con este operador, se puede relacionara los punteros con las variables a ser apuntadas.
 // El * es complementario al &, retorna el valor de la variable cuya direccion es la que apunta. Se opera con el contenido a lo que apunta la variable de tipo puntero.
 //
+//
+//EJEMPLO
 // int main(){
 //    int *ptr;
 //    int dato=300;
@@ -46,15 +51,35 @@
 // !!No hay que confundir el * que aparece en la declaracion con el operador de indireccion !!
 
 
+//ASIGNACION DE PUNTEROS
+//EJEMPLO
+//  int x,*p,*q;
+//  p=&x;           p contiene la direccion de x
+//  q=p;            p contiene la direccion de x
+//                  p y q apuntan a x
+//
+// COMPARACION DE PUNTEROS: == != < <= > >=
+// p<q: compara las direcciones a Ias que apuntan.
+// p==&x: compara la direcciön de memoria a Ia que apunta p con la direcciön de memoria de la variable x
+// p!=q: compara si los punteros p y q apuntan a distintas direcciones de memoria.
 
+
+
+
+
+
+
+//PASAJE A FUNCIONES POR REFERENCIA
 #include <stdio.h>
 
-struct
+void pasoChar()
+void pasoVec
+void pasoInt
 
 
 void main(){
 
-
+v
 
 }
 
