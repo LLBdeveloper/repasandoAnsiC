811aa7796f49573b2ea9a08ef04e5557f35dfc29
Lautaro Bertera <bertera.lautaro@gmail.com>
2025-09-16 22:29:48 -0300

    app perritos final final2

--- punteros.c
+++ punteros.c
@@ -915,8 +915,12 @@ int strlen(char *pString){
 
 ///////////////////////////////////////////////////////////
 
+/*
 
-//Ejemplo en clase 11/9
+//////////////////////////////////////
+////   Ejemplo en clase 11/9   //////
+////////////////////////////////////
+
 
 
 //librerias
@@ -956,19 +960,21 @@ int main(){
     int contPerritos=0;
     int boton;
 
-    /* //harcodeando para probar
-    struct perrito firulais;
-    firulais.id = 1;
-    strcpy(firulais.name,"firulais");
-    firulais.age = 9;
-    strcpy(firulais.adress.street,"alem");
-    firulais.adress.number= 233;
-    dogs[0]= firulais;
-    */
+  //harcodeando para probar
+ //   struct perrito firulais;
+   // firulais.id = 1;
+    //strcpy(firulais.name,"firulais");
+    //firulais.age = 9;
+    //strcpy(firulais.adress.street,"alem");
+   // firulais.adress.number= 233;
+   // dogs[0]= firulais;
 
+
+
+    //menu
 do{
     printf("\n\n\n\n--- MENU ---\n\n\n");
-    printf("\033[34m1. Agregar perrito\n\033[0m");
+    printf("\033[34m1. Agregar perrito\n\033[0m");// \033[34m \033[0m  ES SOLO PARA PONER COLOR AL PRINTF()
     printf("\033[34m2. Ver perritos\n\033[0m");
     printf("\033[34m3. Buscar perrito\n\033[0m");
     printf("\033[34m4. Salir\n\033[0m");
@@ -987,13 +993,16 @@ do{
             break;
         case 4:
             boton=4;
-            system("cls");
+            system("cls"); //limpia consola
             printf("\n \n \n \n \n Saliendo del programa...\n \n \n \n \n");
-            Sleep(2000);
+            Sleep(2000); // efecto delay
+            system("cls"); //limpia consola
+
             break;
         default:
-            system("cls");
-            printf("\n Opcion incorrecta \n");
+            system("cls");//limpia consola
+            printf("\n \n \n \n \n \033[31m- OPCION INCORRECTA - \n\n\n Ingrese entero del 1 al 4 \033[0m\n \n \n \n \n \n");
+
     }
 }while(boton!=4);
 
@@ -1001,15 +1010,22 @@ do{
 }
 
 
-//FUNCIONES
 
-//agrega un perrito
+
+////////////////////////
+//FUNCIONES///////////////////////////////////////////////////
+///////////////////////
+
+
+//
+//FUNCION agrega un perrito
 int add(struct perrito *pVector, int *cantTotal){
-    system("cls");
+    system("cls");//limpia consola
     printf("\n\n\n\n\n AGREGAR PERRITO \n\n\n\n\n");
 
     //creamos instancia nueva
     struct perrito nuevo;
+
 
     //datos del perrix
     //id
@@ -1018,9 +1034,8 @@ int add(struct perrito *pVector, int *cantTotal){
     printf("Ingrese el nombre del perrito: ");
     fflush(stdin);
     fgets(nuevo.name, sizeof(nuevo.name), stdin);
-    //quitar salto de linea
     size_t len = strlen(nuevo.name);
-    if(len > 0 && nuevo.name[len-1] == '\n') nuevo.name[len-1] = '\0';
+    if(len > 0 && nuevo.name[len-1] == '\n') nuevo.name[len-1] = '\0';//quitar salto de linea
     //edad
     printf("Ingrese la edad del perrito: ");
     scanf("%d", &nuevo.age);
@@ -1029,50 +1044,62 @@ int add(struct perrito *pVector, int *cantTotal){
     fflush(stdin);
     fgets(nuevo.adress.street, sizeof(nuevo.adress.street), stdin);
     len = strlen(nuevo.adress.street);
-    if(len > 0 && nuevo.adress.street[len-1] == '\n') nuevo.adress.street[len-1] = '\0';
+    if(len > 0 && nuevo.adress.street[len-1] == '\n') nuevo.adress.street[len-1] = '\0';//quitar salto de linea
     //numero calle
     printf("Ingrese el numero de la calle: ");
     scanf("%d", &nuevo.adress.number);
 
+
     //cargamos al vec la nueva instancia
     pVector[*cantTotal]=nuevo;
 
-    //actualizamos el total
+    //actualizamoss el total
     (*cantTotal)++;
+
+    //feedback
+    system("cls"); //limpia cconsola
+    printf("\n \033[32mPerrito cargado al sistema con exito!!!\033[0m");
+
     return 0;
 };
 
 
-//muestra todos los perritos
+//
+//FUNCION muestra todos los perritos (en este lo hice con acceso por aritmetica de punteros)
 void show(struct perrito *pVector, int cantTotal){
-    system("cls");
+    system("cls");//limpia consola
     printf("\n\n\n\n\n Agenda de perritos \n\n\n\n\n");
+
+    //bucle q muestra los datos del vector dogs
     if(cantTotal!=0){
         for(int i=0; i<cantTotal; i++){
             printf("-Datos del Perrito- \nID:%d \nNombre:%s \nEdad:%d \nDireccion:%s %d\n\n",
-                   pVector[i].id,
-                   pVector[i].name,
-                   pVector[i].age,
-                   pVector[i].adress.street,
-                   pVector[i].adress.number);
-                    }
+                   pVector->id,
+                   pVector->name,
+                   pVector->age,
+                   pVector->adress.street,
+                   pVector->adress.number);
+
+                   pVector++;
+        }
     }else{
-        printf("No hay perritos en sistema!\n\n :(\n");
+        printf("No hay perritos en sistema!\n\n :(\n");//msj error
     }
 };
 
 
-
-
-//busca un perrito por id
+//
+//FUNCION busca un perrito por id
 void search(struct perrito *pVector, int cantTotal){
 
-   int idABuscar;
+    int idABuscar;
     system("cls");
+    //ingreso de id
     printf("\n\n\n\n\n\n BUSQUEDA DE PERRITOS POR ID \n\n\n\n\n\n");
     printf("Ingrese ID del perrito a buscar: \n");
     scanf("%d",&idABuscar);
 
+    //bucle que busca perrix
     for(int i=0; i<cantTotal; i++){
         if(idABuscar==pVector->id){
             system("cls");
@@ -1085,10 +1112,35 @@ void search(struct perrito *pVector, int cantTotal){
         }
         pVector++;
     }
-
-
-
 };
+
+*/
+
+
+/*// opcion B funcion SHOW
+//FUNCION muestra todos los perritos (en este lo hice con acceso por indices) PEOR ?? NO TAN OPTIMO EN TERMINOS DE MEMORIA ??? CONSULTAR
+
+void show(struct perrito *pVector, int cantTotal){
+    system("cls");//limpia consola
+    printf("\n\n\n\n\n Agenda de perritos \n\n\n\n\n");
+
+    //bucle q muestra los datos del vector dogs
+    if(cantTotal!=0){
+        for(int i=0; i<cantTotal; i++){
+            printf("-Datos del Perrito- \nID:%d \nNombre:%s \nEdad:%d \nDireccion:%s %d\n\n",
+                   pVector[i].id,
+                   pVector[i].name,
+                   pVector[i].age,
+                   pVector[i].adress.street,
+                   pVector[i].adress.number);
+                    }
+    }else{
+        printf("No hay perritos en sistema!\n\n :(\n");//msj error
+    }
+};
+*/
+
+
 
 //////////////////////////////////////////////////////
 // COLORES EN TEXTO  //
@@ -1100,7 +1152,3 @@ void search(struct perrito *pVector, int cantTotal){
     // Texto azul
    // printf("\033[34mEste texto es azul\033[0m\n");
 ////////////////////////////////////////////////////////
-
-
-
-
