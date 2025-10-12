faf259e26cf85cf3f70dd6a8fceddc717192dfcd
Lautaro Bertera <bertera.lautaro@gmail.com>
2025-10-12 16:39:39 -0300

    archivos teoria

--- archivos.c
+++ archivos.c
@@ -1,1 +1,22 @@
+//ARCHIVOS  read-white-aniadir
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+    FILE * archivoTXT;
+
+    archivoTXT = fopen("miarchivo.txt","r");
+
+    if(archivoTXT == NULL){
+
+        printf("No se pudo abrir el archivo");
+    } else{
+        printf("El archivo se abrio con exito");
+    }
+
+}
